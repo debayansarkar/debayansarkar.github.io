362a1c35230bbcedb1242c8142e3a46bf3f9f084
Debayan Sarkar <30390444+debayansarkar@users.noreply.github.com>
2017-07-23 23:23:29 +0530

    update1

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+header("access-control-allow-origin: *");
+error_reporting(0);
+$file='data1.txt';
+if( $_GET['data']) {
+	file_put_contents($file, $_GET['data']);
+	echo "success";
+}
+else if( $_GET['show']){
+	$c = file_get_contents($file);
+	echo $c;
+}
+?>
